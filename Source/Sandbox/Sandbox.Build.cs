c4196a43cf20788cf708cacbfc74c47d3a6fd1a9
Michael Iorizzo <miorizzo@hotmail.co.uk>
2022-08-17 14:39:04 +0200

    Submitting new resources and tests and deleted some nasty temp files

---
+++ Source/Sandbox/Sandbox.Build.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class Sandbox : ModuleRules
+{
+	public Sandbox(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
